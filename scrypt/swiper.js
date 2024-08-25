4df5f064757f40393c85ab4cf2ff46427f1891fa
aldm <d44.a1@yandex.ru>
2024-08-25 17:48:27 +0300

    1

--- scrypt/swiper.js
+++ scrypt/swiper.js
@@ -1,16 +1,39 @@
-const swiper = new Swiper(".advantages__swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+var sliderSelector = ".swiper-container",
+  options = {
+    init: false,
+    loop: true,
+    speed: 800,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true,
+    effect: "coverflow",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 60,
+      modifier: 1,
+      slideShadows: true,
+    },
+    grabCursor: true,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1000: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: -80,
+      },
+    },
+  };
+var mySwiper = new Swiper(sliderSelector, options);
+mySwiper.init();
