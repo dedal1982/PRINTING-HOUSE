29c78ced8228a1e55ea75c6cc1cf2989f7ec9852
aldm <d44.a1@yandex.ru>
2024-08-26 20:49:45 +0300

    1

--- scrypt/index.js
+++ scrypt/index.js
@@ -8,3 +8,34 @@ window.addEventListener("scroll", function () {
     header.style.top = "-200px";
   }
 });
+
+function typeText(text) {
+  const textElement = document.createElement("span");
+  const underlineElement = document.createElement("span");
+  underlineElement.textContent = "_";
+  underlineElement.classList.add("blinking-underline");
+
+  let index = 0;
+  const intervalId = setInterval(() => {
+    if (index < text.length) {
+      textElement.textContent += text[index];
+      underlineElement.classList.remove("blinking-underline");
+      void underlineElement.offsetWidth; // Trigger reflow to restart animation
+      underlineElement.classList.add("blinking-underline");
+      index++;
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 200);
+
+  const container = document.createElement("div");
+  container.appendChild(textElement);
+  container.appendChild(underlineElement);
+
+  const typeTextContainer = document.querySelector(".type-text");
+  typeTextContainer.appendChild(container);
+  return container;
+}
+
+// Usage example
+typeText("ЭТИКЕТКИ");
