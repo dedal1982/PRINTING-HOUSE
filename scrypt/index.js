8298a271a87e68555489d786538342a1cd903e9d
aldm <d44.a1@yandex.ru>
2024-08-27 20:14:07 +0300

    1

--- scrypt/index.js
+++ scrypt/index.js
@@ -10,46 +10,44 @@ window.addEventListener("scroll", function () {
 });
 
 function typeText(text) {
-  const textElement = document.createElement("span");
-  const underlineElement = document.createElement("span");
-  underlineElement.textContent = "_";
-  underlineElement.classList.add("blinking-underline");
-
-  let index = 0;
-  let currentText = text;
-  const intervalId = setInterval(() => {
-    if (index < currentText.length) {
-      textElement.textContent += currentText[index];
-      underlineElement.classList.remove("blinking-underline");
-      void underlineElement.offsetWidth;
-      underlineElement.classList.add("blinking-underline");
-      index++;
-    } else {
-      clearInterval(intervalId);
-      const typeTextContainer = document.querySelector(".type-text");
-      typeTextContainer.removeChild(typeTextContainer.lastChild);
-      if (currentText === "ЭТИКЕТКИ") {
-        currentText = "НАКЛЕЙКИ";
-      } else if (currentText === "НАКЛЕЙКИ") {
-        currentText = "ЦЕННИКИ";
-      } else {
-        currentText = "ЭТИКЕТКИ";
-      }
-      typeText(currentText);
-    }
-  }, 200);
-
-  const container = document.createElement("div");
-  container.appendChild(textElement);
-  container.appendChild(underlineElement);
-
   const typeTextContainer = document.querySelector(".type-text");
-  typeTextContainer.appendChild(container);
-  return container;
+  if (document.body.contains(typeTextContainer)) {
+    const textElement = document.createElement("span");
+    const underlineElement = document.createElement("span");
+    underlineElement.textContent = "_";
+    underlineElement.classList.add("blinking-underline");
+
+    let index = 0;
+    let currentText = text;
+    const intervalId = setInterval(() => {
+      if (index < currentText.length) {
+        textElement.textContent += currentText[index];
+        underlineElement.classList.remove("blinking-underline");
+        void underlineElement.offsetWidth;
+        underlineElement.classList.add("blinking-underline");
+        index++;
+      } else {
+        clearInterval(intervalId);
+        typeTextContainer.removeChild(typeTextContainer.lastChild);
+        if (currentText === "ЭТИКЕТКИ") {
+          currentText = "НАКЛЕЙКИ";
+        } else if (currentText === "НАКЛЕЙКИ") {
+          currentText = "ЦЕННИКИ";
+        } else {
+          currentText = "ЭТИКЕТКИ";
+        }
+        typeText(currentText);
+      }
+    }, 200);
+
+    const container = document.createElement("div");
+    container.appendChild(textElement);
+    container.appendChild(underlineElement);
+    typeTextContainer.appendChild(container);
+  }
 }
 
 typeText("ЭТИКЕТКИ");
-
 //функция появления кнопки "ВВЕРХ" и плавное пролистывание страницы вверх
 const buttonUp = document.querySelector(".button-up");
 
