bad62ae39dd77dcde1d6e9c53523cc8c5a3cd5b4
aldm <d44.a1@yandex.ru>
2024-09-06 16:25:40 +0300

    1

--- scrypt/swiper.js
+++ scrypt/swiper.js
@@ -27,38 +27,7 @@ var sliderSelector = ".swiper-container",
       },
     },
   };
-var mySwiper = new Swiper(sliderSelector, options);
-mySwiper.init();
+var mySwipert = new Swiper(sliderSelector, options);
+mySwipert.init();
 
 //////
-var sliderSelectorT = ".swiper-container",
-  options = {
-    init: false,
-    loop: true,
-    speed: 800,
-    slidesPerView: 1,
-    spaceBetween: 0,
-    centeredSlides: true,
-    grabCursor: true,
-    parallax: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      1023: {
-        slidesPerView: 2,
-        spaceBetween: 0,
-      },
-      768: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-      },
-    },
-  };
-var mySwiperT = new Swiper(sliderSelectorT, options);
-mySwiper.init();
