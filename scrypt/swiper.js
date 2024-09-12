4214ddc8c2a9ab5887e2945d362787629633a429
aldm <d44.a1@yandex.ru>
2024-09-12 19:18:45 +0300

    1

--- scrypt/swiper.js
+++ scrypt/swiper.js
@@ -36,3 +36,39 @@ var mySwipert = new Swiper(sliderSelector, options);
 mySwipert.init();
 
 //////
+var sliderSelector2 = ".swiper-container-2",
+  options = {
+    init: false,
+    loop: true,
+    speed: 5000,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true,
+    grabCursor: true,
+    parallax: true,
+    autoplay: {
+      delay: 0,
+      enabled: true,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1023: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+    },
+  };
+var mySwipert2 = new Swiper(sliderSelector2, options);
+mySwipert2.init();
